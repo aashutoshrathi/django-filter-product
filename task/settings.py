c61eac729736df1b299cdd0b756e9e482ea06d9b
Aashutosh Rathi <aashutoshrathi@gmail.com>
2018-06-12 12:43:31 +0530

    More on responsiveness

--- task/settings.py
+++ task/settings.py
@@ -108,7 +108,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
